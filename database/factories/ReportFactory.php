3c5fd26e049994389de84179dc4e0e295a3274cc
Johnson Towoju <johnsontodimu@gmail.com>
2020-05-06 17:19:05 +0100

    feat: add models and seed data

---
+++ database/factories/ReportFactory.php
@@ -0,0 +1,15 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Report;
+use Faker\Generator as Faker;
+
+$factory->define(Report::class, function (Faker $faker) {
+    return [
+        'title' => $faker->sentence,
+        'description' => $faker->text,
+        'votes' => $faker->randomNumber(2),
+        'institution_id' => $faker->numberBetween(1,5)
+    ];
+});
