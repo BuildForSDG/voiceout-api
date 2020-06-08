326b422c12d3e216eb82858e0be488f77decdc2f
Johnson Towoju <johnsontodimu@gmail.com>
2020-06-08 16:11:14 +0100

    fix: Cors error

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*', '127.0.0.1'],
+    'allowed_origins' => ['*', 'http://localhost:3000'],
 
     'allowed_origins_patterns' => [],
 
