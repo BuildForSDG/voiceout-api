9c5cd54befc73aaf972e9fd912e1de7534629d4d
Johnson Towoju <johnsontodimu@gmail.com>
2020-06-09 15:44:44 +0100

    fix: Cors error

--- bootstrap/cache/packages.php
+++ bootstrap/cache/packages.php
@@ -17,6 +17,13 @@
       0 => 'Fideloper\\Proxy\\TrustedProxyServiceProvider',
     ),
   ),
+  'fruitcake/laravel-cors' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Fruitcake\\Cors\\CorsServiceProvider',
+    ),
+  ),
   'intervention/image' => 
   array (
     'providers' => 
